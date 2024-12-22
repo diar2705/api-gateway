ecf777a297c43e9626c08e4fd0c142246e7e3693
sabryassaf <sabry.assaf12@gmail.com>
2024-12-22 19:08:45 +0200

    Address is sent as a string, changed the function to return error in-case of falling

--- controllers/grades.go
+++ controllers/grades.go
@@ -12,19 +12,18 @@ import (
 	"google.golang.org/grpc"
 )
 
-var grpcClient protos.GradesServiceClient
-
-func InitGRPCClient() {
+func InitGRPCClient(address string) (protos.GradesServiceClient, error) {
 	// Initialize the gRPC client connection.
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Failed to connect to Grades Microservice: %v", err)
+		return nil, err
 	}
-	grpcClient = protos.NewGradesServiceClient(conn)
+
+	return protos.NewGradesServiceClient(conn), nil
 }
 
 // GetStudentGradesHandler handles REST requests and calls the gRPC Grades Microservice.
-func GetStudentGradesHandler(c *gin.Context) {
+func GetStudentGradesHandler(c *gin.Context, grpcClient protos.GradesServiceClient) {
 	studentId := c.Param("studentId")
 	courseId := c.Param("courseId")
 
@@ -42,6 +41,7 @@ func GetStudentGradesHandler(c *gin.Context) {
 	if err != nil {
 		log.Printf("Error calling gRPC Grades Microservice: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch grades"})
+
 		return
 	}
 
--- src/main.go
+++ src/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"api-gateway/controllers"
 	"api-gateway/middleware"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	address = "localhost:50051"
+)
+
 func main() {
 	// Initialize the gRPC client connection.
-	controllers.InitGRPCClient()
-
+	grpcClient, err := controllers.InitGRPCClient(address)
+	if err != nil {
+		log.Fatal(err)
+	}
 	router := gin.Default()
 
 	router.Use(middleware.CORSMiddleware())
@@ -18,7 +25,9 @@ func main() {
 	// Rest endpoints.
 	router.POST("/api/login", controllers.LoginHandler)
 	router.GET("/api/dashboard", controllers.GetDashboardData)
-	router.GET("/api/grades/:student_id/:courseId", controllers.GetStudentGradesHandler)
+	router.GET("/api/grades/:student_id/:courseId", func(c *gin.Context) {
+		controllers.GetStudentGradesHandler(c, grpcClient)
+	})
 
 	router.Run(":8080")
 }
